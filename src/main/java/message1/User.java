5c382c292ae487afaa7b7ba8916be3559300766c
Danil Romanov <danillyou@mail.ru>
2024-12-10 17:51:56 +0300

    Проверка 5

--- src/main/java/message1/User.java
+++ src/main/java/message1/User.java
@@ -7,7 +7,10 @@ package message1;
     - пароль
 */
 
-public class User {
-}
 
-123456
+
+public class User {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
