1e0923a0d159806a19f3e724455203483ec690c3
Danil Romanov <danillyou@mail.ru>
2024-12-10 16:52:04 +0300

    Добавил файлы Данил

---
+++ src/main/java/message1/App.java
@@ -0,0 +1,11 @@
+package message1;
+
+/*- Создать 4 пользователя (UserService - create4Users)
+  - Создать 4 сообщения MessageService - create4Message(user1,user2)
+  - Вывести сведения о пользователях
+  - Вывести сведения о сообщениях
+*/
+
+
+public class App {
+}
